5c7498cc32d15bac6f2236d578049321277afb53
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-04-17 15:46:14 -0500

    Update atari_setup.py

--- atari_setup.py
+++ atari_setup.py
@@ -3,4 +3,4 @@ import os
 os.mkdir("emu")
 os.chdir("emu")
 os.system("git clone https://github.com/ppeccin/javatari.js")
-os.system("sudo cp -r javatari.js/release/stable/5.0/standalone/* /var/www/html/javatari")
+os.system("sudo cp -r javatari.js/release/stable/5.0/standalone/* /var/www/html/")
