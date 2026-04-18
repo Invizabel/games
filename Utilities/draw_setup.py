4323b1878c7fbd5db8c9640307dd501289666ad5
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-04-18 12:53:12 -0500

    Update draw_setup.py

--- Utilities/draw_setup.py
+++ Utilities/draw_setup.py
@@ -1,3 +1,8 @@
 import os
 
+os.mkdir("app")
+os.chdir("app")
+os.system("sudo apt update && sudo apt install wget -y")
+os.system("wget https://raw.githubusercontent.com/Invizabel/Scripts/refs/heads/main/Setup/docker_ubuntu.py")
+os.system("python3 docker_ubuntu.py")
 os.system("sudo docker run -d --name=drawio --restart always -p 80:8080 jgraph/drawio")
