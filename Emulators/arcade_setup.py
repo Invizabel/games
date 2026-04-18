0b166dc7787c5721a25581c840b9855ef3ebbdf6
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-04-18 12:39:23 -0500

    Update arcade_setup.py

--- Emulators/arcade_setup.py
+++ Emulators/arcade_setup.py
@@ -3,7 +3,7 @@ import os
 os.mkdir("emu")
 os.chdir("emu")
 os.system("sudo apt update && sudo apt install wget nginx -y")
-os.system("wget https://raw.githubusercontent.com/Invizabel/games/refs/heads/main/index.html")
+os.system("wget https://raw.githubusercontent.com/Invizabel/HomeLab/refs/heads/main/HTML/arcade.html")
 os.system("wget https://raw.githubusercontent.com/Invizabel/Scripts/refs/heads/main/Setup/docker_ubuntu.py")
 os.system("python3 docker_ubuntu.py")
 os.system("sudo docker run -d --name=webrcade -p 8080:80 -p 8443:443 --restart always webrcade/webrcade:latest")
