0a8aec8aa02d922f5b0776e600b85df5013b47e7
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-04-19 00:18:43 -0500

    Update food_setup.py

--- Lifestyle/food_setup.py
+++ Lifestyle/food_setup.py
@@ -3,6 +3,6 @@ import os
 os.mkdir("app")
 os.chdir("app")
 os.system("sudo apt update && sudo apt install wget -y")
-os.system("wget https://raw.githubusercontent.com/Invizabel/Scripts/refs/heads/main/Setup/docker_ubuntu.py")
+os.system("wget https://raw.githubusercontent.com/Invizabel/HomeLab/refs/heads/main/Assets/docker_setup.py")
 os.system("python3 docker_ubuntu.py")
 os.system("sudo docker run -d --name=mealie --restart always -p 80:9000 ghcr.io/mealie-recipes/mealie:latest")
