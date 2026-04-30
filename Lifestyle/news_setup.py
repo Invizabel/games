3ce665b8f70f7511c365bdc2d5993b571353e394
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-04-30 08:36:28 -0500

    Update news_setup.py

--- Lifestyle/news_setup.py
+++ Lifestyle/news_setup.py
@@ -2,7 +2,7 @@ import os
 
 os.mkdir("app")
 os.chdir("app")
-os.system("sudo apt update")
+os.system("sudo apt update && sudo apt install wget -y")
 os.system("wget https://raw.githubusercontent.com/Invizabel/HomeLab/refs/heads/main/Assets/docker_setup.py")
 os.system("python3 docker_setup.py")
 os.system("sudo docker run -d -p 80:80 -e TZ=America/Chicago -e 'CRON_MIN=1,31' -v freshrss_data:/var/www/FreshRSS/data -v freshrss_extensions:/var/www/FreshRSS/extensions --name freshrss --restart always ghcr.io/freshrss/freshrss")
